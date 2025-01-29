92933a7f60b7de824d2d8fe02259283339f94bd9
kemsguy7 <mattidungafa@gmail.com>
2025-01-29 18:31:37 +0100

    updates: imported index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
+import './index.css'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import abi from './abi.json'
